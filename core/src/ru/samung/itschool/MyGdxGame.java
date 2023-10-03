d40cb291cc855dba5b3f040e786cf2c0132c69e4
glebpav <glebpavlyuk@gamil.com>
2023-10-03 16:21:10 +0300

    topic_1 implementation

--- core/src/ru/samung/itschool/MyGdxGame.java
+++ core/src/ru/samung/itschool/MyGdxGame.java
@@ -1,31 +1,63 @@
 package ru.samung.itschool;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
 
 public class MyGdxGame extends ApplicationAdapter {
 	SpriteBatch batch;
-	Texture img;
+	Texture birdTexture;
+
+	int birdX, birdY;
+	int birdSpeed = 5;
+
+	int birdWidth = 250;
+	int birdHeight = 200;
+
+	boolean up = true;
+
+	int screenHeight, screenWidth;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		birdTexture = new Texture("bird0.png");
+
+		birdX = 0;
+		birdY = 0;
+
+		screenHeight = Gdx.graphics.getHeight();
+		screenWidth = Gdx.graphics.getWidth();
+
 	}
 
 	@Override
 	public void render () {
+
+		if (birdY >= screenHeight - birdHeight) {
+			up = false;
+		}
+		if (birdY <= 0) {
+			up = true;
+		}
+
+		if (up) {
+			birdY += birdSpeed;
+		} else {
+			birdY -= birdSpeed;
+		}
+
 		ScreenUtils.clear(1, 0, 0, 1);
 		batch.begin();
-		batch.draw(img, 0, 0);
+		batch.draw(birdTexture, birdX, birdY, birdWidth, birdHeight);
 		batch.end();
 	}
 	
 	@Override
 	public void dispose () {
 		batch.dispose();
-		img.dispose();
+		birdTexture.dispose();
 	}
 }
