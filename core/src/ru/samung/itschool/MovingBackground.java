6020b3aaa8784d292b3be84db82f60c96153c435
glebpav <glebpavlyuk@gamil.com>
2023-10-07 11:00:29 +0300

    better naming

--- core/src/ru/samung/itschool/MovingBackground.java
+++ core/src/ru/samung/itschool/MovingBackground.java
@@ -7,30 +7,30 @@ public class MovingBackground {
 
     Texture texture;
 
-    int x, x2;
+    int texture1X, texture2X;
     int speed = 3;
 
     public MovingBackground(String pathToTexture) {
-        x = 0;
-        x2 = MyGdxGame.SCR_WIDTH;
+        texture1X = 0;
+        texture2X = MyGdxGame.SCR_WIDTH;
         texture = new Texture(pathToTexture);
     }
 
     public void move() {
-        x -= speed;
-        x2 -= speed;
+        texture1X -= speed;
+        texture2X -= speed;
 
-        if (x <= -MyGdxGame.SCR_WIDTH) {
-            x = MyGdxGame.SCR_WIDTH;
+        if (texture1X <= -MyGdxGame.SCR_WIDTH) {
+            texture1X = MyGdxGame.SCR_WIDTH;
         }
-        if (x2 <= -MyGdxGame.SCR_WIDTH) {
-            x2 = MyGdxGame.SCR_WIDTH;
+        if (texture2X <= -MyGdxGame.SCR_WIDTH) {
+            texture2X = MyGdxGame.SCR_WIDTH;
         }
     }
 
     public void draw(Batch batch) {
-        batch.draw(texture, x, 0, MyGdxGame.SCR_WIDTH + 2, MyGdxGame.SCR_HEIGHT);
-        batch.draw(texture, x2, 0, MyGdxGame.SCR_WIDTH + 2, MyGdxGame.SCR_HEIGHT);
+        batch.draw(texture, texture1X, 0, MyGdxGame.SCR_WIDTH + 2, MyGdxGame.SCR_HEIGHT);
+        batch.draw(texture, texture2X, 0, MyGdxGame.SCR_WIDTH + 2, MyGdxGame.SCR_HEIGHT);
     }
 
     public void dispose() {
