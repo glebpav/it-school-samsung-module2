cec8b9e5f492e5c7596b58d5031073e6905738f9
glebpav <glebpavlyuk@gmail.com>
2023-10-27 19:29:18 +0300

    initial commit

--- core/src/ru/samung/itschool/MyGdxGame.java
+++ core/src/ru/samung/itschool/MyGdxGame.java
@@ -7,32 +7,26 @@ import com.badlogic.gdx.utils.ScreenUtils;
 
 public class MyGdxGame extends ApplicationAdapter {
 	SpriteBatch batch;
-	Texture birdTexture;
+	Texture img;
 
-	int birdX = 0, birdY = 0;
-	int birdSpeed = 5;
-	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		birdTexture = new Texture("bird0.png");
+		img = new Texture("badlogic.jpg");
 	}
 
 	@Override
 	public void render () {
-
-		birdX += birdSpeed;
-		birdY += birdSpeed;
-
 		ScreenUtils.clear(1, 0, 0, 1);
 		batch.begin();
-		batch.draw(birdTexture, birdX, birdY);
+		batch.draw(img, 0, 0);
 		batch.end();
 	}
-	
+
 	@Override
 	public void dispose () {
 		batch.dispose();
-		birdTexture.dispose();
+		img.dispose();
 	}
 }
+
