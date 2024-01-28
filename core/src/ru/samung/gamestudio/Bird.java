6d3419d06eb1fb92a8289139b78c2c3139c2a993
glebpav <glebpavlyuk@gmail.com>
2024-01-28 13:56:11 +0300

    some refactoring

--- core/src/ru/samung/gamestudio/Bird.java
+++ core/src/ru/samung/gamestudio/Bird.java
@@ -6,10 +6,10 @@ import com.badlogic.gdx.graphics.g2d.Batch;
 public class Bird {
 
     int x, y;
-    int with, height;
+    int width, height;
 
     int speed;
-    float jumpHeight;
+    int jumpHeight;
     final int maxHeightOfJump = 200;
     boolean jump = true;
 
@@ -20,7 +20,7 @@ public class Bird {
         this.x = x;
         this.y = y;
         this.speed = speed;
-        this.with = birdWith;
+        this.width = birdWith;
         this.height = birdHeight;
         frameCounter = 0;
 
@@ -51,7 +51,7 @@ public class Bird {
 
     void draw(Batch batch) {
         int frameMultiplier = 10;
-        batch.draw(framesArray[frameCounter / frameMultiplier], x, y, with, height);
+        batch.draw(framesArray[frameCounter / frameMultiplier], x, y, width, height);
         if (frameCounter++ == framesArray.length * frameMultiplier - 1) frameCounter = 0;
     }
 
