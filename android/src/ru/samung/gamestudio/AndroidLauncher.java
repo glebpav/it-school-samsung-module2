76921a79e3a7fbb70532f8b36474d19f82cd478a
glebpav <glebpavlyuk@gmail.com>
2024-01-28 13:14:10 +0300

    some refactoring

--- android/src/ru/samung/gamestudio/AndroidLauncher.java
+++ android/src/ru/samung/gamestudio/AndroidLauncher.java
@@ -1,10 +1,9 @@
-package ru.samung.itschool;
+package ru.samung.gamestudio;
 
 import android.os.Bundle;
 
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
-import ru.samung.itschool.MyGdxGame;
 
 public class AndroidLauncher extends AndroidApplication {
 	@Override
